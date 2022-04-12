914dbe228bb33b280c2452237b7c1ea14d2979e0
Jobin Jacob Paily <jobinjacobpaily@gmail.com>
2022-04-12 13:18:56 +0530

    initial commit

---
+++ src/main/java/com/spring/boot/admin/admin/config/SpringSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.spring.boot.admin.admin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/**")
+                .hasRole("ADMIN")
+                .and().formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}admin")
+                .roles("ADMIN");
+    }
+}
